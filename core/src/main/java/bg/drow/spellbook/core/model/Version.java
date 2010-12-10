554da51c8c310b51c28571b36f2af44b74871335
Bozhidar Batsov <lordbad@gmail.com>
2010-12-10 02:33:51 +0200

    minor improvements

--- core/src/main/java/bg/drow/spellbook/core/model/Version.java
+++ core/src/main/java/bg/drow/spellbook/core/model/Version.java
@@ -12,41 +12,11 @@ public class Version implements Comparable<Version> {
     private int minor;
     private int revision;
 
-    public Version(int major, int minor, int revision) {
-        this.major = major;
-        this.minor = minor;
-        this.revision = revision;
-    }
-
     public Version(String versionString) {
         String[] version = versionString.split("\\.");
         major = Integer.parseInt(version[0]);
         minor = Integer.parseInt(version[1]);
         revision = Integer.parseInt(version[2]);
-    }
-
-    public int getMajor() {
-        return major;
-    }
-
-    public void setMajor(int major) {
-        this.major = major;
-    }
-
-    public int getMinor() {
-        return minor;
-    }
-
-    public void setMinor(int minor) {
-        this.minor = minor;
-    }
-
-    public int getRevision() {
-        return revision;
-    }
-
-    public void setRevision(int revision) {
-        this.revision = revision;
     }
 
     @Override
@@ -58,7 +28,7 @@ public class Version implements Comparable<Version> {
         } else if (major == other.major && minor == other.minor && revision == other.revision) {
             return 0;
         } else {
-            return 0;
+            return 1;
         }
     }
 
